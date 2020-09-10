40af9ba31c4e427d70e3ac9eb668cbf824177f59
Yusup88 <62787757+Yusup88@users.noreply.github.com>
2020-09-10 02:39:36 +0000

    Create bs.py

---
+++ bs.py
@@ -0,0 +1,16 @@
+#by Yusup Yusup
+#Recode gw pukul pala lu
+
+import time
+import random
+import os
+
+os.system('clear')
+os.system("figlet Whatsupp |lolcat")
+nmr = raw_input('Masukan Nomor Target : ')
+time.sleep(3)
+kode = random.choice(["4461","9877","2037","5670","4786","7881","0799","0768","9882"])
+time.sleep(3)
+print "[*] Succes baypass verifikasi whatsapp Kntl"
+time.sleep(2)
+print "Kode Verif : "+kode
